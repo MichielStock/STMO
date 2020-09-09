db7673d9f1cc3461a4e0d9d7e03470fe48822dac
michielstock <michielfmstock@gmail.com>
2020-09-09 14:49:27 +0200

    :clock: update dates etc

--- build.jl
+++ build.jl
@@ -13,21 +13,22 @@ t₀ = time()
 using Weave
 
 
-dirs = [#="00.Introduction",
+dirs = ["00.Introduction",
         "01.Brackets",
         "02.Quadratic",
-        "03.Unconstrained",
-        "04.Constrained",
-        "05.AutoDiff",
+        "03.AutoDiff",
+        "04.Unconstrained",
+        "05.Constrained",
         "06.OptimalTransport",
         "07.MST",
-        "08.ShortestPath",=#
+        "08.ShortestPath",
         "09.NP-Complete",
+        "10.Metaheuristics",
         "11.TSP"
         ]
 
 
-function cleanTemps(filename, dir; exts=[".out", ".log", ".aux"])
+function cleanTemps(filename, dir; exts=[".out", ".log", ".aux", ".fls", ".fdb_latexmk"])
   for ext in exts
     rm(dir * filename * ext)
   end
@@ -40,9 +41,9 @@ for dir in dirs
   for filename in files
     filename = filename[1:end-4]
     println("Building $(filename)...")
-    weave(dir * filename * ".jmd"; doctype="md2pdf")
-    cleanTemps(filename, dir)
-    convert_doc(dir * filename * ".jmd", dir * filename * ".ipynb")
+    weave(joinpath(dir, filename * ".jmd"); doctype="md2pdf")
+    #cleanTemps(filename, dir)
+    convert_doc(joinpath(dir, filename * ".jmd"), dir * filename * ".ipynb")
   end
 end
 
